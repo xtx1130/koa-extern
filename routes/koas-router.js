491776f2932544cf54925eb152b049ad4a02fcb2
xtx1130 <xtx1130@gmail.com>
2017-06-07 18:12:38 +0800

    add router

--- routes/koas-router.js
+++ routes/koas-router.js
@@ -1,5 +1,13 @@
 'use strict';
+
 const KoaRouter = require('koa-router');
+const routerConf = require('../koasConfig').router;
+const path = require('path');
+const staticRoute = {};
+for(let i in routerConf){
+	staticRoute[i] = require(path.join(__dirname,'../',routerConf[i]));
+}
+console.log(staticRoute)
 class KoasRouter extends KoaRouter{
 	constructor(){
 		super();
