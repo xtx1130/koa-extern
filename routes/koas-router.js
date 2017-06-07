5aa7d5e166e55f598a3ceba2cd8041f75251b0a8
xtx1130 <xtx1130@gmail.com>
2017-06-07 18:21:31 +0800

    change file

--- routes/koas-router.js
+++ routes/koas-router.js
@@ -4,6 +4,7 @@ const KoaRouter = require('koa-router');
 const routerConf = require('../koasConfig').router;
 const path = require('path');
 const staticRoute = {};
+const routes = Symbol.for('koas#routes');
 for(let i in routerConf){
 	staticRoute[i] = require(path.join(__dirname,'../',routerConf[i]));
 }
@@ -11,7 +12,7 @@ console.log(staticRoute)
 class KoasRouter extends KoaRouter{
 	constructor(){
 		super();
-
+		this[routes] = staticRoute;
 	}
 }
 console.log(KoasRouter)
