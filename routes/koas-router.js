ed4effcd9764511decd307035e403c301542c839
xtx1130 <xtx1130@gmail.com>
2017-06-08 14:35:08 +0800

    add routesMap

--- routes/koas-router.js
+++ routes/koas-router.js
@@ -5,9 +5,12 @@ const routerConf = require('../koasConfig').router;
 const path = require('path');
 const delFirst = require('../deps/delFirstLetter');
 const addLast = require('../deps/addLastLetter');
+
 const staticRoute = {};
 const routes = Symbol.for('koas#routes');
 const memoryRoutes = Symbol.for('koas#memoryRoutes');
+const routesMap = Symbol.for('koas#routesMap');
+
 for(let i in routerConf){
 	staticRoute[i] = require(path.join(__dirname,'../',routerConf[i]));
 }
@@ -16,14 +19,17 @@ class KoasRouter extends KoaRouter{
 		super();
 		this[routes] = staticRoute;
 		this[memoryRoutes] = {};
+		this[routesMap] = [];
 	}
 	trueUri() {
 		let [tem,temBase] = [{},'/'];
 		for(let i in this[routes]){
 			this[routes][i].baseRouter&&(temBase = this[routes][i].baseRouter);
+			this[routesMap].push(addLast(temBase));
 			Object.values(this[routes][i]).forEach(ob => {
 				if(ob['url']){
 					ob['url'] = addLast(temBase)+delFirst(ob['url']);
+					this[routesMap].push(ob['url'])
 				}
 			})
 		}
@@ -34,9 +40,10 @@ class KoasRouter extends KoaRouter{
 		[this[memoryRoutes][router],this[routes][router]] = [this[routes][router],null]
 	}
 	addRouter(router) {
-		assert(this[routes][router]!=null,'This router is not exists');
-		this[routes][router] = [this[memoryRoutes][router]
+		assert(this[memoryRoutes][router]!=null,'This router is not exists');
+		this[routes][router] = this[memoryRoutes][router];
 	}
+
 }
 let s = new KoasRouter();
-console.log(s.trueUri())
+console.log(s.trueUri(),s[routesMap])
