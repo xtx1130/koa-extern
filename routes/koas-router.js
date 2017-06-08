fbb40139cfef637e130aba0efc7472936d337f4e
xtx1130 <xtx1130@gmail.com>
2017-06-08 10:53:02 +0800

    add returns

--- routes/koas-router.js
+++ routes/koas-router.js
@@ -3,12 +3,13 @@
 const KoaRouter = require('koa-router');
 const routerConf = require('../koasConfig').router;
 const path = require('path');
+const delFirst = require('../deps/delFirstLetter');
+const addLast = require('../deps/addLastLetter');
 const staticRoute = {};
 const routes = Symbol.for('koas#routes');
 for(let i in routerConf){
 	staticRoute[i] = require(path.join(__dirname,'../',routerConf[i]));
 }
-console.log(staticRoute)
 class KoasRouter extends KoaRouter{
 	constructor(){
 		super();
@@ -19,11 +20,13 @@ class KoasRouter extends KoaRouter{
 		for(let i in this[routes]){
 			this[routes][i].baseRouter&&(temBase = this[routes][i].baseRouter);
 			Object.values(this[routes][i]).forEach(ob => {
-				if(ob[url]){
-					console.log(1)
+				if(ob['url']){
+					ob['url'] = addLast(temBase)+delFirst(ob['url'])
 				}
 			})
 		}
+		return this[routes];
 	}
 }
-console.log(KoasRouter)
+let s = new KoasRouter();
+console.log(s.trueUri())
