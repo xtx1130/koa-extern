1f050df09f888c539cf4ca427dce26085557b18d
xtx1130 <xtx1130@gmail.com>
2017-06-08 17:02:23 +0800

    change test case

--- test/test.js
+++ test/test.js
@@ -7,9 +7,9 @@ module.exports.test = callback => {
 	tests.routes = callback =>{
 		let koasRoutes = new routesTest(true);
 		testing.verify(Array.isArray(koasRoutes.map),'koasRoutes.map must be an array',callback);
-		testing.verify(typeof koasRoutes.trueUri() === 'object','koasRoutes.trueUri() must be a Object',callback);
-		console.log(koasRoutes.trueUri())
-		//koasRoutes.deleteRouter('list/test1')
+		testing.verify(typeof koasRoutes.jsonMap === 'object','koasRoutes.trueUri() must be a Object',callback);
+		console.log(koasRoutes.jsonMap,koasRoutes.map)
+		koasRoutes.deleteRouter('list','test1')
 		//testing.verify(koasRoutes.trueUri()['test1'] === null,'koasRoutes.trueUri() must be a Object',callback);
 		testing.success(callback);
 	}
