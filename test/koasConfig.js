6da987e2807429007a9716665b2b73a274645808
xtx1130 <xtx1130@gmail.com>
2017-06-08 15:31:28 +0800

    add test case and travis.ci

---
+++ test/koasConfig.js
@@ -0,0 +1,10 @@
+'use strict'
+module.exports = {
+	router:{
+		movie:'/test/movie/router.js',
+		list:'/test/list/router.js'
+	},
+	controller:{
+		test:'test/controller/index.js'
+	}
+}
---
+++ test/test.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const testing = require('testing');
+module.exports.test = callback => {
+	let tests = {};
+	let routesTest = require('../routes/koas-router');
+	tests.routes = callback =>{
+		let koasRoutes = new routesTest(true);
+		testing.verify(Array.isArray(koasRoutes.map),'koasRoutes.map must be an array',callback);
+		testing.verify(typeof koasRoutes.trueUri() === 'object','koasRoutes.trueUri() must be a Object',callback);
+		console.log(koasRoutes.trueUri())
+		//koasRoutes.deleteRouter('list/test1')
+		//testing.verify(koasRoutes.trueUri()['test1'] === null,'koasRoutes.trueUri() must be a Object',callback);
+		testing.success(callback);
+	}
+	testing.run(tests, 1000, callback);
+}
+exports.test(testing.show);
