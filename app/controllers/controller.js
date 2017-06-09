7bf00794eac89abf4f1bce2327a617d9b13d2097
xtx1130 <xtx1130@gmail.com>
2017-06-09 16:46:31 +0800

    open double controller test

--- app/controllers/controller.js
+++ app/controllers/controller.js
@@ -4,16 +4,21 @@
 */
 'use strict';
 
+const path = require('path');
 const controllerMap = Symbol.for('koas#controllerMap');
 
 let controllerConf = require('../../koasConfig').controller;
 class Controller {
 	constructor(isTest){
-		isTest&&controllerConf = require('../../test/koasConfig').controller;
+		isTest&&(controllerConf = require('../../test/koasConfig').controller);
 		this[controllerMap] = {};
 		for(var i in controllerConf){
-			this[controllerMap][i] = controllerConf[i];
+			this[controllerMap][i] = require(path.join(__dirname,'../../',controllerConf[i]));
 		}
+	}
+	get map() {
+		return this[controllerMap] 
 	}
 
 }
+module.exports = Controller;
