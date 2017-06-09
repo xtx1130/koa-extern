dd9676faa6f012ab33b974849e5f64640158864b
xtx1130 <xtx1130@gmail.com>
2017-06-09 14:34:08 +0800

    add controller

--- app/controllers/controller.js
+++ app/controllers/controller.js
@@ -2,3 +2,18 @@
  *@author: xietianxin@qiyi.com
  *@description: 整合projects下面的controller文件夹
 */
+'use strict';
+
+const controllerMap = Symbol.for('koas#controllerMap');
+
+let controllerConf = require('../../koasConfig').controller;
+class Controller {
+	constructor(isTest){
+		isTest&&controllerConf = require('../../test/koasConfig').controller;
+		this[controllerMap] = {};
+		for(var i in controllerConf){
+			this[controllerMap][i] = controllerConf[i];
+		}
+	}
+
+}
