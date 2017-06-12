9c2cf18e7e33327b6984542b9ddbd5dbf995cf71
xtx1130 <xtx1130@gmail.com>
2017-06-12 12:06:17 +0800

    establish controller and routes

--- index.js
+++ index.js
@@ -4,8 +4,8 @@
  *@description 处理基础routes和基本controller绑定，抛出扩展后的koa构造函数
  */
 const Koa = require('koa');
-const Routes = require('app/routes/koas-router');
-const Controller = require('app/controllers/controller');
+const Routes = require('./app/routes/koas-router');
+const Controller = require('./app/controllers/controller');
 class Koas extends Koa{
 	constructor(){
 		super();
@@ -13,3 +13,5 @@ class Koas extends Koa{
 		this.controller = new Controller();
 	}
 }
+let s = new Koas();
+console.dir(s)
