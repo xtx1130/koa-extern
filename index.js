df2b6512eb4c5f5eec378bd8eccce34149a33e2b
xtx1130 <xtx1130@gmail.com>
2017-06-12 11:55:29 +0800

    add core

--- index.js
+++ index.js
@@ -4,9 +4,12 @@
  *@description 处理基础routes和基本controller绑定，抛出扩展后的koa构造函数
  */
 const Koa = require('koa');
+const Routes = require('app/routes/koas-router');
+const Controller = require('app/controllers/controller');
 class Koas extends Koa{
 	constructor(){
 		super();
-
+		this.routes = new Routes();
+		this.controller = new Controller();
 	}
 }
