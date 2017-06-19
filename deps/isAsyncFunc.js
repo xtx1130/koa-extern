1f0b8cadd5917c1e947655da48c15cd0d1bf90fd
xtx1130 <xtx1130@gmail.com>
2017-06-19 14:56:59 +0800

    add async function depends file

--- deps/isAsyncFunc.js
+++ deps/isAsyncFunc.js
@@ -1,3 +1,8 @@
+/*
+ *@description ensure the func is async function or not
+ *@author xtx1130
+ *@return {boolean} ture\false
+*/
 'use strict';
 module.exports = func => {
 	return Object.prototype.toString.call(func) === '[object AsyncFunction]'
