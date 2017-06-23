5f3ecf9f0c04457ef5646a988aaa9d2cafb3451d
xtx1130 <xtx1130@gmail.com>
2017-06-23 10:53:54 +0800

    add comments

--- test/test.js
+++ test/test.js
@@ -80,6 +80,9 @@ module.exports.test = callback => {
 		testserver.close((error) => {
 			testing.check(error, 'Could not stop server', callback);
 		});
+		/*daily build 需要改一下注释啊，别tmd忘了。。。要不然又failing了
+		改天写env的时候记得写个dailybuild的env
+		*/
 		// server.close((error) => {
 		// 	testing.check(error, 'Could not stop server', callback);
 		// 	testing.success(callback);
