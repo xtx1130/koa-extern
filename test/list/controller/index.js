a15431ce315726fab8331baad51ebe7d7be96937
xtx1130 <xtx1130@gmail.com>
2017-06-23 15:16:32 +0800

    add test cookie

--- test/list/controller/index.js
+++ test/list/controller/index.js
@@ -1,8 +1,13 @@
+'use strict';
+const tough = require('tough-cookie');
+const Cookie = tough.Cookie;
 module.exports = {
 	listtest1:require('./test1'),
 	listtest2:require('./test2'),
 	index:async (ctx,next)=>{
+		console.log('````````````')
 		ctx.body = ctx.koax;
+		ctx.cookies.set('xtx','nb')
 		ctx.status =200;
 		await next();
 	}
