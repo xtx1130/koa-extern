2238eb29052013f0da700b6303c22c7929d56ab4
xtx1130 <xtx1130@gmail.com>
2017-06-23 15:30:55 +0800

    add converage

--- test/test.js
+++ test/test.js
@@ -83,11 +83,11 @@ module.exports.test = callback => {
 		/*daily build 需要改一下注释啊，别tmd忘了。。。要不然又failing了
 		改天写env的时候记得写个dailybuild的env
 		*/
-		// server.close((error) => {
-		// 	testing.check(error, 'Could not stop server', callback);
-		// 	testing.success(callback);
-		// });
-		testing.success(callback);
+		server.close((error) => {
+			testing.check(error, 'Could not stop server', callback);
+			testing.success(callback);
+		});
+		//testing.success(callback);
 	}
 	testing.run(tests, 1000, callback);
 }
