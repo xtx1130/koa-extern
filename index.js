30c8526a5f7972d3c0bf5c8fe3ac68cbc4eb33fe
xtx1130 <xtx1130@gmail.com>
2017-07-04 15:16:16 +0800

    change assert for controller

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ class Koas extends Koa {
 			for (let j in this.routesMap[i]) {
 				if (j === 'baseRouter') {
 					//对一级路由进行绑定 统一get方法
-					Assert(isAsync(this.controlMap[i].index), 'controlMap\'s index must be an async function');
+					Assert(isAsync(this.controlMap[i].index), `${i} index must be an async function`);
 					this.koasroutes.get(this.routesMap[i][j], this.controlMap[i].index);
 				} else {
 					//对二级路由进行绑定，方法为routesMap中的方法，没有的话默认get
