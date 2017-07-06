6f642383ff3bb048ea1d78eb8dc94910f59a5990
xtx1130 <xtx1130@gmail.com>
2017-07-06 19:45:16 +0800

    add description for test files

--- test/test.js
+++ test/test.js
@@ -11,68 +11,73 @@ const err = require('../app/middleware/koas_error');
 let tests = {};
 let routesTest = require('../app/routes/koas-router');
 let controllerTest = require('../app/controllers/controller');
-let Koas = require('../index');
+let Koas = require('../');
 //koas-router 测试
-
-let koasRoutes = new routesTest(true);
-koasRoutes.map = 1; //对set map()进行测试
-koasRoutes.jsonMap = 1; //对set jsonMap()进行测试
-test('koasRoutes.map must be an Array', () => {
-	expect(Array.isArray(koasRoutes.map)).toBe(true)
-});
-test('koasRoutes.jsonMap must be an Object', () => {
-	expect(typeof koasRoutes.jsonMap === 'object').toBe(true);
-});
-let testMapDel = () => {
-	for (let i = 0; i < koasRoutes.map.length; i++) {
-		if (koasRoutes.map[i] == '')
-			return true
+describe('koas-router tester',() => {
+	let koasRoutes = new routesTest(true);
+	koasRoutes.map = 1; //对set map()进行测试
+	koasRoutes.jsonMap = 1; //对set jsonMap()进行测试
+	test('koasRoutes.map must be an Array', () => {
+		expect(Array.isArray(koasRoutes.map)).toBe(true)
+	});
+	test('koasRoutes.jsonMap must be an Object', () => {
+		expect(typeof koasRoutes.jsonMap === 'object').toBe(true);
+	});
+	let testMapDel = () => {
+		for (let i = 0; i < koasRoutes.map.length; i++) {
+			if (koasRoutes.map[i] == '')
+				return true
+		}
+		return false;
 	}
-	return false;
-}
-test('one of the koa router has been deleted',() => {
-	koasRoutes.deleteRouter('list', 'listtest1');
-	//console.log(koasRoutes.map) //测试的时候看结构用
-	expect(koasRoutes.jsonMap.list.listtest1.status == 0).toBe(true)
-});
-test('deleteRouter is ok',()=>{
-	expect(testMapDel()).toBe(true);
-});
-let testMapAdd = () => {
-	for (let i = 0; i < koasRoutes.map.length; i++) {
-		if (koasRoutes.map[i] == '')
-			return false
+	test('one of the koa router has been deleted',() => {
+		koasRoutes.deleteRouter('list', 'listtest1');
+		//console.log(koasRoutes.map) //测试的时候看结构用
+		expect(koasRoutes.jsonMap.list.listtest1.status == 0).toBe(true)
+	});
+	test('deleteRouter is ok',()=>{
+		expect(testMapDel()).toBe(true);
+	});
+	let testMapAdd = () => {
+		for (let i = 0; i < koasRoutes.map.length; i++) {
+			if (koasRoutes.map[i] == '')
+				return false
+		}
+		return true;
 	}
-	return true;
-}
-test('addRouter is ok ,router has been added', () => {
-	koasRoutes.addRouter('list', 'listtest1');
-	expect(koasRoutes.jsonMap.list.listtest1.status).toBe(1)
-});
-test('add function has been finished', () => {
-	expect(testMapAdd()).toBe(true)
+	test('addRouter is ok ,router has been added', () => {
+		koasRoutes.addRouter('list', 'listtest1');
+		expect(koasRoutes.jsonMap.list.listtest1.status).toBe(1)
+	});
+	test('add function has been finished', () => {
+		expect(testMapAdd()).toBe(true)
+	});
 });
 //koas controller 测试
-
-let koasController = new controllerTest(true);
-test('koasController.map must be an Object', () => {
-	koasController.jsonMap = 1; //对set jsonMap()进行测试
-	expect(typeof koasController.jsonMap).toBe('object')
-});
-//console.log(koasController.jsonMap);
-test('slot must return a function or async function', ()=>{
-	let koasSlot = koasController.slot('movie', 'movietest1')
-	expect(typeof koasSlot).toBe('function');
+describe('koas controller tester',() => {
+	let koasController = new controllerTest(true);
+	test('koasController.map must be an Object', () => {
+		koasController.jsonMap = 1; //对set jsonMap()进行测试
+		expect(typeof koasController.jsonMap).toBe('object')
+	});
+	//console.log(koasController.jsonMap);
+	test('slot must return a function or async function', ()=>{
+		let koasSlot = koasController.slot('movie', 'movietest1')
+		expect(typeof koasSlot).toBe('function');
+	});
 });
 //koas 测试
-/*let app = new Koas(true);
-app.use(async(ctx, next) => {
-	throw new Error('wtf')
-	await next();
-})
-let server = app.listen('8011');
-if (process.env.NODE_ENV === 'travis') {
-	server.close(error => {
-		testing.check(error, 'Could not stop server', callback);
-	});
-}*/
+console.dir(new Koas())
+//let app = Koas(true);
+// app.use(async(ctx, next) => {
+// 	throw new Error('wtf')
+// 	await next();
+// })
+// let server = app.listen('8011');
+// if (process.env.NODE_ENV === 'travis') {
+// 	server.close(error => {
+// 		test('Could not stop server',() => {
+// 			expect(error).toBe('')
+// 		});
+// 	});
+// }
