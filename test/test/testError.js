91284f9ef609edf06a49c75e4fb450cbedc58304
xtx1130 <xtx1130@gmail.com>
2017-09-18 16:32:44 +0800

    add test case

--- test/test/testError.js
+++ test/test/testError.js
@@ -17,13 +17,19 @@ describe('koas server error test 500',() => {
 	let app = new Koas();
 	app.use(async (ctx, next) => {
 		var err = new Error('error test');
-		err.code = '500 inter error';
+		err.code = '500';
+		ctx.type = ctx.request.type;
 		throw err;
 		await next();
 	});
 	let server = app.listen('8012');
 	it('if 8012 port request 500 error',async () => {
-		expect.assertions(2);
+		expect.assertions(3);
+		try{
+			await requestAction('text/plain','8012')
+		}catch(e){
+			expect(e.statusCode).toEqual(500);
+		}
 		try{
 			await requestAction('text/html','8012')
 		}catch(e){
@@ -51,14 +57,14 @@ describe('koas server error test 404',() => {
 	it('if 8013 port request 404 error',async () => {
 		expect.assertions(2);
 		try{
-			await requestAction('text/html','8013')
+			await requestAction('application/json','8013')
 		}catch(e){
-			//console.log(e)
 			expect(e.statusCode).toEqual(404);
 		}
 		try{
-			await requestAction('application/json','8013')
+			await requestAction('text/plain','8013')
 		}catch(e){
+			//console.log(e)
 			expect(e.statusCode).toEqual(404);
 		}
 		server.close(error => {
