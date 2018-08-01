0b120047d7f03679e520beaee9922c66fe512770
xtx1130 <xtx1130@gmail.com>
2018-08-01 16:44:18 +0800

    test: node v8.x has support async func

--- test/test/testAsyncMiddleWare.js
+++ test/test/testAsyncMiddleWare.js
@@ -6,7 +6,8 @@ const isAsyncFunc = require('../../deps/isAsyncFunc')
 describe('establish async middleware',() => {
   let normalFunc = (...args) => {}
   test('normalFunc should return AsyncFunction',() => {
-    //https://github.com/caolan/async can't support function named AsyncFunction
-    expect(isAsyncFunc(asyncMiddleware(normalFunc))).toBe(false)
+    // https://github.com/caolan/async can't support function named AsyncFunction
+    // node v8.x has supportted 
+    expect(isAsyncFunc(asyncMiddleware(normalFunc))).toBe(true)
   })
 })
